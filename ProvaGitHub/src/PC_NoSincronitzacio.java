c468c623eeb34fe6cf058b4fc9f8e5f1e8ee17b6
Eric Gutierrez <Eric@172.24.1.12>
2014-01-23 18:21:09 +0100

    versió 1

--- ProvaGitHub/src/PC_NoSincronitzacio.java
+++ ProvaGitHub/src/PC_NoSincronitzacio.java
@@ -5,8 +5,9 @@ public class PC_NoSincronitzacio {
 		Buffer b = new Buffer();
 		Productor p = new Productor(b);
 		Consumidor c = new Consumidor(b);
-		
+		Consumidor c1 = new Consumidor(b);
 		p.start();
 		c.start();
+		c1.start();
 	}
 }
